31e89037f985893f652e059f86a49ed1c366fdf2
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-12-13 09:03:17 -0800

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:learning/models/user.dart';
-import 'package:learning/widgets/EnterHomePage.dart';
+import 'package:learning/widgets/EnterHomePage.dart'; 
 
 import 'screens/my_home_page.dart';
 
